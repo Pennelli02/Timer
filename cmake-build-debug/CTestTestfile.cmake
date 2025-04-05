9c4e8f21c735bd39d88c306e4957c209fcf6577f
lorenzo pennelli <161378352+Pennelli02@users.noreply.github.com>
2025-04-05 18:25:26 +0200

    Remove cmake-build-debug directory from version control

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Asus/CLionProjects/Timer
-# Build directory: C:/Users/Asus/CLionProjects/Timer/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Timer_test]=] "C:/Users/Asus/CLionProjects/Timer/cmake-build-debug/Timer_test.exe")
-set_tests_properties([=[Timer_test]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Asus/CLionProjects/Timer/CMakeLists.txt;103;add_test;C:/Users/Asus/CLionProjects/Timer/CMakeLists.txt;0;")
